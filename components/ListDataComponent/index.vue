9e1057d62d3c993134aca736554f75e249d68c1c
yo.chen[陳友銓] <yo.chen@104.com.tw>
2020-12-07 13:57:08 +0800

    test pretice for first time

---
+++ components/ListDataComponent/index.vue
@@ -0,0 +1,24 @@
+
+<script>
+export default {
+  name: 'ListDataComponent',
+  props: {
+    listData: {
+      type: Object,
+      default: {}
+    }
+  },
+  data() {
+    return {
+        isEdit: false
+    }
+  },
+  methods: {
+      doEdit() {
+          this.isEdit = !this.isEdit;
+      }
+  }
+}
+</script>
+
+<template src="./template.html" />
---
+++ pages/index.vue
@@ -0,0 +1,70 @@
+
+<template>
+  <div>
+    <client-only>
+      <div id="fb-root"></div>
+      <div class="fb-comments" data-href="http://localhost:3000/" data-numposts="5" data-width=""></div>
+      </client-only>
+  </div>
+</template>
+
+
+<script>
+import Vue from 'vue';
+export default {
+  data() {
+    return {
+      post: []
+    }
+  },
+  mounted() {
+    window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '761351597756340',
+      xfbml      : true,
+      version    : 'v8.0'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/zh_TW/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+  },
+  head() {
+    return {
+      title: '',
+      meta: [
+                {
+          property: 'fb:app_id',
+          content: '761351597756340'
+        },
+        {
+          property: 'og:url',
+          content: 'URL'
+        },
+        {
+          property: 'og:title',
+          content: 'TITLE'
+        },
+        {
+          property: 'og:description',
+          content: 'DESC'
+        },
+        {
+          property: 'og:image',
+          content: 'IMG'
+        },
+        {
+          property: 'fb:admins',
+          content: '2047278791957536'
+        }
+      ]
+    }
+  },
+}
+</script>
