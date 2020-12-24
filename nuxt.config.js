cc30a1835eb457b177447dcce847a733b5291cb8
yo.chen[陳友銓] <yo.chen@104.com.tw>
2020-12-24 16:27:40 +0800

    取消 fb-sdk

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    { src: '~/plugins/fb-sdk', ssr: false }
+    // { src: '~/plugins/fb-sdk', ssr: false }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
