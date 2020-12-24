b543fce640ee89c31eb1c5003ccb608f53a60ba1
yo.chen[陳友銓] <yo.chen@104.com.tw>
2020-12-24 15:46:54 +0800

    gogo 加入 iframe

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,10 @@
       <p>test3</p>
       <p>{{ fullPath }}</p>
       <div class="fb-comments" @data-href="fullPath" data-numposts="5" data-width=""></div>
+      <iframe
+        style="height: 140px; width: 100%; border: none; border-radius: 4px; box-shadow: 0 1px 8px rgba(0, 0, 0, .2);"
+        src="https://player.soundon.fm/embed?podcast=5d485b3f-7d08-42de-8f0e-dd3496dcb5bc&episode=8e11d151-0c9f-4a75-b0c0-2001ec9ba1de">
+      </iframe>
   </div>
 </template>
 
