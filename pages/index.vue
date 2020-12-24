21e7dd4619e57237429fe31dbe17732452a4ad16
yo.chen[陳友銓] <yo.chen@104.com.tw>
2020-12-24 15:54:59 +0800

    測試 sound on 順便測試 js-load

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 
 <template>
   <div>
+    <script v-html="jsonld" type="application/ld+json"></script>
       <div id="fb-root"></div>
-      <p>test3</p>
-      <p>{{ fullPath }}</p>
-      <div class="fb-comments" @data-href="fullPath" data-numposts="5" data-width=""></div>
       <iframe
         style="height: 140px; width: 100%; border: none; border-radius: 4px; box-shadow: 0 1px 8px rgba(0, 0, 0, .2);"
         src="https://player.soundon.fm/embed?podcast=5d485b3f-7d08-42de-8f0e-dd3496dcb5bc&episode=8e11d151-0c9f-4a75-b0c0-2001ec9ba1de">
       </iframe>
+      <div class="fb-comments" @data-href="fullPath" data-numposts="5" data-width=""></div>
   </div>
 </template>
 
@@ -17,7 +16,17 @@
 import Vue from 'vue';
 export default {
   data() {
+    const jsonld = {
+      '@type': 'PodcastSeries',
+      webFeed: 'https://soundcloud.com/104senior/',
+      image: require('~/assets/image/homePage/icon_logo.svg'),
+      url: 'https://senior.104.com.tw/events/podcast/',
+      name: '高年級不打烊',
+      description: '聽人物、說工作。每個高年級朋友經歷職場上的退休，猶如一年四季的換季。退休後轉換了不同的心情和形式，經歷不一樣的人生和工作。主持人是賽桑，5年級2班，每一集會邀請不同的高年級朋友，分享他們的換季和退休經驗談。',
+      author: { name: 'name' }
+    }
     return {
+      jsonld,
       fullPath: this.$route.fullPath
     }
   },
