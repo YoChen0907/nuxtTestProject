bfe82f4c133b7c91705e7f257ca808f46919b7d2
yo.chen[陳友銓] <yo.chen@104.com.tw>
2020-12-24 16:04:31 +0800

    gogo

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div>
-    <script v-html="jsonld" type="application/ld+json"></script>
+    <!-- <script v-html="jsonld" type="application/ld+json"></script> -->
       <div id="fb-root"></div>
       <iframe
         style="height: 140px; width: 100%; border: none; border-radius: 4px; box-shadow: 0 1px 8px rgba(0, 0, 0, .2);"
@@ -18,17 +18,16 @@ export default {
   data() {
     const jsonld = {
       '@type': 'PodcastSeries',
-      webFeed: 'https://soundcloud.com/104senior/',
-      image: require('~/assets/image/homePage/icon_logo.svg'),
-      url: 'https://senior.104.com.tw/events/podcast/',
-      name: '高年級不打烊',
-      description: '聽人物、說工作。每個高年級朋友經歷職場上的退休，猶如一年四季的換季。退休後轉換了不同的心情和形式，經歷不一樣的人生和工作。主持人是賽桑，5年級2班，每一集會邀請不同的高年級朋友，分享他們的換季和退休經驗談。',
-      author: { name: 'name' }
+      webFeed: 'https://main.d3u4rdok1doiis.amplifyapp.com/',
+      url: 'https://main.d3u4rdok1doiis.amplifyapp.com/',
+      name: '不打烊',
+      description: '測試 js-onload',
+      author: { name: 'authorName' }
     }
-    return {
+    return ({
       jsonld,
       fullPath: this.$route.fullPath
-    }
+    })
   },
   updated() {
     const FB = Vue.FB;
