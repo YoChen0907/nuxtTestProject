f205eefe0c78a7e3f0c8eb30832ad8cc1722bc7a
yo.chen[陳友銓] <yo.chen@104.com.tw>
2020-12-24 17:29:28 +0800

    test 啥都沒有

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
   <div>
     <!-- <script v-html="jsonld" type="application/ld+json"></script> -->
       <div id="fb-root"></div>
-      <iframe
+      <!-- <iframe
         style="height: 140px; width: 100%; border: none; border-radius: 4px; box-shadow: 0 1px 8px rgba(0, 0, 0, .2);"
         src="https://player.soundon.fm/embed?podcast=5d485b3f-7d08-42de-8f0e-dd3496dcb5bc&episode=8e11d151-0c9f-4a75-b0c0-2001ec9ba1de">
-      </iframe>
+      </iframe> -->
       <div class="fb-comments" @data-href="fullPath" data-numposts="5" data-width=""></div>
   </div>
 </template>
