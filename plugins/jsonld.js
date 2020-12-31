74aaa5b40d8cc4a08aeed55ade376612e1a1c680
yo.chen[陳友銓] <yo.chen@104.com.tw>
2020-12-31 14:47:53 +0800

    測試 js on ld

--- plugins/jsonld.js
+++ plugins/jsonld.js
@@ -5,7 +5,3 @@ import NuxtJsonld from 'nuxt-jsonld';
 
 Vue.use(NuxtJsonld);
 
-// you can set the indentation
-Vue.use(NuxtJsonld, {
-  space: process.env.NODE_ENV === 'production' ? 0 : 2 // default: 2
-});
